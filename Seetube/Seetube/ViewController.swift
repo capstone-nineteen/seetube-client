adc4d301d147721f2e87c255095cac99253de845
최수정 <babeeboo2000@gmail.com>
2023-01-11 20:43:33 +0900

    [Init] 프로젝트 생성

---
+++ Seetube/Seetube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
