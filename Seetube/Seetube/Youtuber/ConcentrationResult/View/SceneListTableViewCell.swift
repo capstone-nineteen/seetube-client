703bfe22fa745c3f72aaa83f45d786c60883333d
최수정 <babeeboo2000@gmail.com>
2023-01-13 00:17:35 +0900

    [Feat] #2 유튜버 결과 리스트 뷰 구현

---
+++ Seetube/Seetube/Youtuber/ConcentrationResult/View/SceneListTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SceneListTableViewCell.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/01/12.
+//
+
+import UIKit
+
+class SceneListTableViewCell: UITableViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var timeIntervalLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Seetube/Seetube/Youtuber/ConcentrationResult/View/SceneListView.swift
@@ -0,0 +1,35 @@
+//
+//  SceneListView.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/01/12.
+//
+
+import UIKit
+
+@IBDesignable
+class SceneListView: UIView, NibLoadable {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBInspectable var title: String? {
+        set { self.titleLabel.text = newValue }
+        get { return self.titleLabel.text }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.loadFromNib(owner: self)
+        self.configureTableView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.loadFromNib(owner: self)
+        self.configureTableView()
+    }
+    
+    private func configureTableView() {
+        // TODO: cell register
+    }
+}
