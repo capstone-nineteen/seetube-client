96ecb6ef60fd1ea5f57ea990804f058f18527ff2
최수정 <babeeboo2000@gmail.com>
2023-01-20 02:48:54 +0900

    [Feat] #7 로그인화면 Segmented Control 커스텀으로 변경

--- Seetube/Seetube/Common/Login/LoginViewController.swift
+++ Seetube/Seetube/Common/Login/LoginViewController.swift
@@ -13,7 +13,6 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureSegmentedControl()
         self.changeToReviewerMode()
     }
 
@@ -40,23 +39,11 @@ class LoginViewController: UIViewController {
 }
 
 extension LoginViewController {
-    private func configureSegmentedControl() {
-        let backgroundImage = UIImage()
-        //self.segmentedControl.setBackgroundImage(backgroundImage, for: .normal, barMetrics: .default)
-        self.segmentedControl.setBackgroundImage(backgroundImage, for: .highlighted, barMetrics: .default)
-        self.segmentedControl.setBackgroundImage(backgroundImage, for: .selected, barMetrics: .default)
-        
-        self.segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.systemGray, .font: UIFont.systemFont(ofSize: 14)], for: .normal)
-        self.segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(named: "AccentColor") ?? UIColor.systemPink, .font: UIFont.systemFont(ofSize: 15, weight: .semibold)], for: .selected)
-    }
-}
-
-extension LoginViewController {
     private func changeToReviewerMode() {
         self.guidanceLabel.text = "원하는 컨텐츠를 시청하고\n리워드를 적립해보세요"
     }
     
     private func changeToYoutuberMode() {
-        self.guidanceLabel.text = "내 유튜브 동영상에"
+        self.guidanceLabel.text = "내 유튜브 동영상에 대한\n사람들의 반응을 확인해보세요"
     }
 }
