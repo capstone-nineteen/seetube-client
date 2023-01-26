3a9517d20e45dbd36233a8764d40a6794ea1b1b8
최수정 <babeeboo2000@gmail.com>
2023-01-27 01:14:38 +0900

    [Refactor] #15 리뷰어 공통 비디오 정보 테이블뷰 컨트롤러 추가

--- Seetube/Seetube/Reviewer/Home/SearchResultTableViewController.swift
+++ Seetube/Seetube/Reviewer/Home/SearchResultTableViewController.swift
@@ -7,21 +7,12 @@
 
 import UIKit
 
-class SearchResultTableViewController: VideoInfoCardTableViewController {
+class SearchResultTableViewController: ReviewerVideoInfoTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func registerCell() {
-        self.tableView.register(ReviewerVideoInfoTableViewCell.self, forCellReuseIdentifier: ReviewerVideoInfoTableViewCell.cellReuseIdentifier)
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 7
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ReviewerVideoInfoTableViewCell.cellReuseIdentifier, for: indexPath) as? ReviewerVideoInfoTableViewCell else { return UITableViewCell() }
-        return cell
     }
 }
---
+++ Seetube/Seetube/Reviewer/Reusable/ViewController/ReviewerVideoInfoTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReviewerVideoInfoTableViewController.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/01/27.
+//
+
+import UIKit
+
+class ReviewerVideoInfoTableViewController: VideoInfoCardTableViewController {
+    override func registerCell() {
+        self.tableView.register(ReviewerVideoInfoTableViewCell.self, forCellReuseIdentifier: ReviewerVideoInfoTableViewCell.cellReuseIdentifier)
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ReviewerVideoInfoTableViewCell.cellReuseIdentifier, for: indexPath) as? ReviewerVideoInfoTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+}
