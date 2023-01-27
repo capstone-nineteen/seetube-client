60879c70ecdf9f0168f3c19deb4281de66c08e8c
최수정 <babeeboo2000@gmail.com>
2023-01-28 01:38:35 +0900

    [Feat] #19 리뷰어 홈화면 컬렉션뷰 셀 커스텀

---
+++ Seetube/Seetube/Reviewer/Home/View/ReviewerHomeCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ReviewerHomeCollectionViewCell.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/01/28.
+//
+
+import UIKit
+
+class ReviewerHomeCollectionViewCell: UICollectionViewCell {
+    static let cellReuseIdentifier = "ReviewerHomeCollectionViewCell"
+    
+    private lazy var videoInfoCardView = VideoInfoCardView()
+    private lazy var priceAccessoryView = PriceAccessoryView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configureSubviews()
+        self.configureAccessoryView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configureSubviews()
+        self.configureAccessoryView()
+    }
+    
+    private func configureSubviews() {
+        self.addSubview(self.videoInfoCardView)
+        self.videoInfoCardView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.videoInfoCardView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.videoInfoCardView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.videoInfoCardView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.videoInfoCardView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+    }
+    
+    private func configureAccessoryView() {
+        self.videoInfoCardView.configureAccessoryView(self.priceAccessoryView)
+    }
+}
