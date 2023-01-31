e35c731a368ca8df8e99bdd837626eb6e13ed223
최수정 <babeeboo2000@gmail.com>
2023-01-31 17:12:26 +0900

    [Feat] #21 AppDelegate handleURL 추가

--- Seetube/Seetube/AppDelegate.swift
+++ Seetube/Seetube/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,6 +32,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
 
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+
+        // Handle other custom URL types.
+
+        // If not handled by this app, return false.
+        return false
+    }
 }
 
