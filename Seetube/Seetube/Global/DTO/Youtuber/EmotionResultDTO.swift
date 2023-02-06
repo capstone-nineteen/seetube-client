cf914c8d66303b84eaf65b888c3f90c49ff24f70
최수정 <babeeboo2000@gmail.com>
2023-02-06 18:01:17 +0900

    [Feat] #23 감정 분석결과 화면 DTO 생성

---
+++ Seetube/Seetube/Global/DTO/Youtuber/EmotionResultDTO.swift
@@ -0,0 +1,22 @@
+//
+//  EmotionResultDTO.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/02/06.
+//
+
+import Foundation
+
+struct EmotionSceneDTO: Decodable {
+    let thumbnailImageURL: String
+    let startTime: Int
+    let endTime: Int
+    let totalNumberOfReviewers: Int
+    let numberOfReviewersFelt: Int
+    let emotionType: Emotion
+}
+
+struct EmotionResultDTO: Decodable {
+    let originalVideoURL: String
+    let scenes: [ConcentrationSceneDTO]
+}
---
+++ Seetube/Seetube/Global/Model/Emotion.swift
@@ -0,0 +1,18 @@
+//
+//  Emotion.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/02/06.
+//
+
+import Foundation
+
+enum Emotion: String, Codable {
+    case angry = "분노"
+    case disgust = "불쾌"
+    case fear = "공포"
+    case happy = "행복"
+    case sad = "슬픔"
+    case surprise = "놀람"
+    case neutral = "중립"
+}
