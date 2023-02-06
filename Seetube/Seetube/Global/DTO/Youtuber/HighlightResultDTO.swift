8448799f856f4c07447969e78b80fe7e9bdd9aad
최수정 <babeeboo2000@gmail.com>
2023-02-06 18:09:16 +0900

    [Feat] #23 하이라이트 추천결과 화면 DTO 생성

---
+++ Seetube/Seetube/Global/DTO/Youtuber/HighlightResultDTO.swift
@@ -0,0 +1,22 @@
+//
+//  HighlightResultDTO.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/02/06.
+//
+
+import Foundation
+
+struct HighlightSceneDTO: Decodable {
+    let thumbnailImageURL: String
+    let startTime: Int
+    let endTime: Int
+    let totalNumberOfReviewers: Int
+    let numberOfReviewersConcentrated: Int
+    let numberOfReviewersFelt: Int
+    let emotionType: Emotion
+}
+
+struct HighlightResultDTO: Decodable {
+    let scenes: [HighlightResultDTO]
+}
