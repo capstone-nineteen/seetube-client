4923b2ff35f6d412ae8c71dc72486f4639f43534
최수정 <babeeboo2000@gmail.com>
2023-02-06 17:14:47 +0900

    [Feat] #23 검색결과 화면 DTO 생성

---
+++ Seetube/Seetube/Global/DTO/Reviewer/SearchResultDTO.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultDTO.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/02/06.
+//
+
+import Foundation
+
+struct SearchResultDTO: Decodable {
+    let videos: [VideoInfoCardDTO]
+}
