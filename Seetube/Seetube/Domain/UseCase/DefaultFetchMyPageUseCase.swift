173d137d789913571bfd73cd528d6779586ec296
최수정 <babeeboo2000@gmail.com>
2023-02-10 00:22:34 +0900

    [Feat] #22 FetchMyPageUseCase 정의

---
+++ Seetube/Seetube/Domain/UseCase/DefaultFetchMyPageUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  DefaultFetchMyPageUseCase.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/02/09.
+//
+
+import Foundation
+import RxSwift
+
+class DefaultFetchMyCaseUseCase: FetchMyPageUseCase {
+    private let repository: MyPageRepository
+    
+    init(repository: MyPageRepository) {
+        self.repository = repository
+    }
+    
+    func execute() -> Observable<MyPage> {
+        return self.repository.getMyPage()
+    }
+}
---
+++ Seetube/Seetube/Domain/UseCase/Protocol/FetchMyPageUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  FetchMyPageUseCase.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/02/09.
+//
+
+import Foundation
+import RxSwift
+
+protocol FetchMyPageUseCase {
+    func execute() -> Observable<MyPage>
+}
