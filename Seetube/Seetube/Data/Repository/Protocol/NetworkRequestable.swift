08eccc7aeade79f2ff10fffc28d89c3b6f0fae20
최수정 <babeeboo2000@gmail.com>
2023-02-09 19:46:26 +0900

    [Feat] #25 NetworkRequestable 프로토콜 정의

---
+++ Seetube/Seetube/Data/Repository/Protocol/NetworkRequestable.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkRequestable.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/02/09.
+//
+
+import Foundation
+import RxSwift
+
+protocol NetworkRequestable {
+    func getResource<T: DTO>(endpoint: Endpoint, decodingType: T.Type) -> Observable<T.DomainModel>
+}
+
+extension NetworkRequestable {
+    func getResource<T: DTO>(endpoint: Endpoint, decodingType: T.Type) -> Observable<T.DomainModel> {
+        NetworkService
+            .request(endpoint)
+            .retry(3)
+            .map { try JSONDecoder().decode(T.self, from: $0) }
+            .map { $0.toDomain() }
+    }
+}
