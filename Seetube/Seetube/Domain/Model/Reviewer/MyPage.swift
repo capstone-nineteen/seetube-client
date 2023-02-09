ad5157ec93e9f327ebdaddfbed3132bee5bd7a04
최수정 <babeeboo2000@gmail.com>
2023-02-10 00:21:22 +0900

    [Feat] #22 MyPage 모델 정의

--- Seetube/Seetube/Domain/Model/Reviewer/MyPage.swift
+++ Seetube/Seetube/Domain/Model/Reviewer/MyPage.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+enum CoinHistoryType {
+    case earn
+    case use
+}
+
+struct CoinHistory {
+    let date: Date
+    let content: String
+    let type: CoinHistoryType
+    let amount: Int
+}
+
 struct MyPage {
+    let name: String
+    let coin: Int
+    let coinHistories: [CoinHistory]
     
+    init(name: String = "",
+         coin: Int = 0,
+         coinHistories: [CoinHistory] = []
+    ) {
+        self.name = name
+        self.coin = coin
+        self.coinHistories = coinHistories
+    }
 }
