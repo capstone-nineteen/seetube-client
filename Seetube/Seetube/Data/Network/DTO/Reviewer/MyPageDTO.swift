cc890a9db22b60557f491bece658aa46f26c2e76
최수정 <babeeboo2000@gmail.com>
2023-02-14 17:50:09 +0900

    [Fix] #22 API 변경 대응

--- Seetube/Seetube/Data/Network/DTO/Reviewer/MyPageDTO.swift
+++ Seetube/Seetube/Data/Network/DTO/Reviewer/MyPageDTO.swift
@@ -7,30 +7,46 @@
 
 import Foundation
 
-struct CoinHistoryDTO: Decodable {
-    let date: Date
-    let content: String
-    let amount: Int
+struct WithdrawHistoryDTO: Decodable {
+    let withdrawCoin: Int
+    let withdrawDate: Date
     
-    func toDomain(coinHistoryType: CoinHistoryType) -> CoinHistory {
-        return CoinHistory(date: self.date,
-                           content: self.content,
-                           type: coinHistoryType,
-                           amount: self.amount)
+    func toDomain() -> CoinHistory {
+        return CoinHistory(date: self.withdrawDate,
+                           content: "환급",
+                           type: .use,
+                           amount: self.withdrawCoin)
+    }
+}
+
+struct ReviewHistoryDTO: Decodable {
+    struct Video: Decodable {
+        let videoTitle: String
+        let videoCoin: Int
+    }
+    
+    let reviewDate: Date
+    let video: Video
+    
+    func toDomain() -> CoinHistory {
+        return CoinHistory(date: self.reviewDate,
+                           content: self.video.videoTitle,
+                           type: .earn,
+                           amount: self.video.videoCoin)
     }
 }
 
 struct MyPageDTO: Decodable {
     let name: String
     let coin: Int
-    let withdrawHistories: [CoinHistoryDTO]
-    let reviewHistories: [CoinHistoryDTO]
+    let withdrawHistories: [WithdrawHistoryDTO]
+    let reviewHistories: [ReviewHistoryDTO]
 }
 
 extension MyPageDTO: DomainConvertible {
     private func combineAllHistories() -> [CoinHistory] {
-        let usageHistories = self.withdrawHistories.map { $0.toDomain(coinHistoryType: .use) }
-        let earningHistories = self.reviewHistories.map { $0.toDomain(coinHistoryType: .earn) }
+        let usageHistories = self.withdrawHistories.map { $0.toDomain() }
+        let earningHistories = self.reviewHistories.map { $0.toDomain() }
         return usageHistories + earningHistories
     }
     
