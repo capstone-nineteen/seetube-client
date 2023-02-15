c651c1af999c477a58e1a65b7aef3f1e2efa3127
최수정 <babeeboo2000@gmail.com>
2023-02-15 19:09:18 +0900

    [Feat] #29 VideoDetailView bind 함수 구현

--- Seetube/Seetube/Presentation/Common/View/VideoDetailView.swift
+++ Seetube/Seetube/Presentation/Common/View/VideoDetailView.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Kingfisher
 
 @IBDesignable
 class VideoDetailView: UIView, NibLoadable {
     @IBOutlet weak var thumbnailButton: VideoThumbnailButton!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var youtuberNameLabel: UILabel!
+    @IBOutlet weak var rewardLabel: UILabel!
     @IBOutlet weak var rewardPriceStackView: UIStackView!
+    @IBOutlet weak var reviewProgressView: ReviewProgressView!
     @IBOutlet weak var reviewPeriodLabel: UILabel!
     @IBOutlet weak var videoDescriptionLabel: UILabel!
     @IBOutlet weak var hashtagLabel: UILabel!
@@ -34,4 +39,36 @@ class VideoDetailView: UIView, NibLoadable {
     
     func configureBottomButtonName() { }
     func configureRewardPriceStackView() { }
+    
+    func bind(with viewModel: Driver<VideoDetailViewModel>) -> Cancelable {
+        return Disposables.create (
+            viewModel
+                .map { $0.title }
+                .drive(with: self) { obj, url in
+                    obj.thumbnailButton.kf
+                        .setImage(with: URL(string: url),
+                                  for: .normal)
+                },
+            viewModel
+                .map { $0.title }
+                .drive(self.titleLabel.rx.text),
+            viewModel
+                .map { $0.youtuber }
+                .drive(self.youtuberNameLabel.rx.text),
+            viewModel
+                .map { $0.reward }
+                .drive(self.rewardLabel.rx.text),
+            self.reviewProgressView
+                .bind(with: viewModel.map { $0.progressViewModel }),
+            viewModel
+                .map { $0.reviewPeriod }
+                .drive(self.reviewPeriodLabel.rx.text),
+            viewModel
+                .map { $0.videoDescription }
+                .drive(self.videoDescriptionLabel.rx.text),
+            viewModel
+                .map { $0.hashtags }
+                .drive(self.hashtagLabel.rx.text)
+        )
+    }
 }
