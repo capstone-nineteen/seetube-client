4f7ebe212a3319d50ce09acb3ac489bd6ef02005
최수정 <babeeboo2000@gmail.com>
2023-02-18 02:49:31 +0900

    [Feat] #31 화면 진입 시 텍스트필드 초기화 기능 추가

--- Seetube/Seetube/Presentation/Reviewer/Shop/ViewModel/ShopViewModel.swift
+++ Seetube/Seetube/Presentation/Reviewer/Shop/ViewModel/ShopViewModel.swift
@@ -26,9 +26,12 @@ class ShopViewModel: ViewModelType {
             }
             .compactMap { $0 }
             .map { $0.totalCoinAmount }
-        // TODO: view will appear 때마다 텍스트필드 초기화
             
-        let withdrawal = input.withdrawalAmountChanged
+        let withdrawal = Driver
+            .merge(
+                input.viewWillAppear.map { _ in "" },
+                input.withdrawalAmountChanged
+            )
             .distinctUntilChanged()
             .map { $0 == "" ? "0" : $0 }
             .map { $0.replacingOccurrences(of: ",", with: "") }
