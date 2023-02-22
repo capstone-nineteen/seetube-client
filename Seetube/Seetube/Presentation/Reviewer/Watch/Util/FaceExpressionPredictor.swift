301d7b2aa11107fedbbf0ba1f8f9f8a986a990d4
최수정 <babeeboo2000@gmail.com>
2023-02-22 22:57:36 +0900

    [Feat] #37 표정 분석 메소드 구현

--- Seetube/Seetube/Presentation/Reviewer/Watch/Util/FaceExpressionPredictor.swift
+++ Seetube/Seetube/Presentation/Reviewer/Watch/Util/FaceExpressionPredictor.swift
@@ -7,8 +7,20 @@
 
 import Foundation
 import Vision
+import RxSwift
+import CoreImage
+
+enum FaceExpressionPredictionError: Error {
+    case faceDetectionFailed
+    case faceCroppingFailed
+    case faceExpressionClassificationFailed
+    case noPredictions
+    case wrongPredictionType
+}
 
 class FaceExpressionPredictor {
+    // MARK: - Static
+    
     static func createFaceExpressionClassifier() -> VNCoreMLModel {
         guard let miniXception = try? MiniXception(configuration: MLModelConfiguration()),
               let miniXceptionVisionModel = try? VNCoreMLModel(for: miniXception.model)
@@ -26,47 +38,124 @@ class FaceExpressionPredictor {
         let confidencePercentage: Int
     }
     
-    typealias FaceExpressionPredictionHandler = (_ predications: [Prediction]?) -> Void
+    // MARK: - Instance
     
-    private func createFaceExpressionClassificationRequest(predictionHandler: @escaping FaceExpressionPredictionHandler) -> VNImageBasedRequest {
-        let faceExpressionClassificationRequest = VNCoreMLRequest(model: Self.faceExpressionClassifier) { (request, error) in
-            var predictions: [Prediction]? = nil
+    func makePredictions(for image: CVPixelBuffer) -> Observable<Prediction> {
+        return self.detectFace(in: image)
+            .withUnretained(self)
+            .flatMap { obj, observations in
+                obj.cropFace(from: image, with: observations)
+            }
+            .withUnretained(self)
+            .flatMap { obj, cgImage in
+                obj.classifyFaceExpression(cgImage)
+            }
+    }
+    
+    /// 카메라 프레임으로부터 얼굴을 탐지한다.
+    private func detectFace(in image: CVPixelBuffer) -> Observable<[VNFaceObservation]> {
+        return Observable<[VNFaceObservation]>.create { observer in
+            let faceDetectionRequest = VNDetectFaceLandmarksRequest(completionHandler: { (request: VNRequest, error: Error?) in
+                if let results = request.results as? [VNFaceObservation] {
+                    observer.onNext(results)
+                } else {
+                    observer.onError(FaceExpressionPredictionError.faceDetectionFailed)
+                }
+                observer.onCompleted()
+            })
+            let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: image,
+                                                            orientation: .down,
+                                                            options: [:])
             
-            defer {
-                predictionHandler(predictions)
+            DispatchQueue.global(qos: .userInitiated).async {
+                do {
+                    try imageRequestHandler.perform([faceDetectionRequest])
+                } catch {
+                    observer.onError(error)
+                }
             }
             
+            return Disposables.create()
+        }
+    }
+    
+    /// 얼굴 탐지 결과를 바탕으로 얼굴 부분만 크롭한다.
+    private func cropFace(from image: CVPixelBuffer, with observedFaces: [VNFaceObservation]) -> Observable<CGImage> {
+        return Observable<CGImage>.create { observer in
+            if let observedFace = observedFaces.first {
+                let ciImage = CIImage(cvPixelBuffer: image).transformed(by: CGAffineTransform(rotationAngle: .pi))
+                let cgImage = CIContext().createCGImage(ciImage, from: ciImage.extent)
+
+                if let croppedImage = cgImage?.croppingDetectionBondingBox(to: observedFace.boundingBox) {
+                    observer.onNext(croppedImage)
+                } else {
+                    observer.onError(FaceExpressionPredictionError.faceCroppingFailed)
+                }
+                observer.onCompleted()
+            } else {
+                observer.onError(FaceExpressionPredictionError.faceDetectionFailed)
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    /// 얼굴 이미지에 대해 표정 분석을 수행한다.
+    private func classifyFaceExpression(_ image: CGImage) -> Observable<Prediction> {
+        return Observable<Prediction>.create { [weak self] observer in
+            guard let self = self else { return Disposables.create() }
+            
+            let faceExpressionClassificaitonRequest = self.createFaceExpressionClassificationRequest { result in
+                switch result {
+                case .success(let prediction):
+                    observer.onNext(prediction)
+                case .failure(let error):
+                    observer.onError(error)
+                }
+                observer.onCompleted()
+            }
+            let requests: [VNRequest] = [faceExpressionClassificaitonRequest]
+            let handler = VNImageRequestHandler(cgImage: image)
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                do {
+                    try handler.perform(requests)
+                } catch {
+                    observer.onError(error)
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    typealias FaceExpressionPredictionHandler = (_ predications: Result<Prediction, Error>) -> Void
+    
+    /// CoreML Request를 생성한다.
+    private func createFaceExpressionClassificationRequest(predictionHandler: @escaping FaceExpressionPredictionHandler) -> VNImageBasedRequest {
+        let faceExpressionClassificationRequest = VNCoreMLRequest(model: Self.faceExpressionClassifier) { (request, error) in
             if let error = error {
-                print("Vision image classification error...\n\n\(error.localizedDescription)")
+                predictionHandler(.failure(error))
                 return
             }
             
             if request.results == nil {
-                print("Vision request had no resuls")
+                predictionHandler(.failure(FaceExpressionPredictionError.noPredictions))
                 return
             }
             
-            guard let observations = request.results as? [VNClassificationObservation] else {
-                print("VNRequest produced the wrong result type: \(type(of: request.results))")
+            guard let observations = request.results as? [VNClassificationObservation],
+                  let observation = observations.first else {
+                predictionHandler(.failure(FaceExpressionPredictionError.wrongPredictionType))
                 return
             }
             
-            predictions = observations.map { observation in
-                Prediction(classification: Emotion(rawValue: observation.identifier) ?? .neutral,
-                           confidencePercentage: Int(observation.confidence * 100))
-            }
+            let prediction = Prediction(classification: Emotion(rawValue: observation.identifier) ?? .neutral,
+                                        confidencePercentage: Int(observation.confidence * 100))
+            predictionHandler(.success(prediction))
         }
         
         faceExpressionClassificationRequest.imageCropAndScaleOption = .centerCrop
         return faceExpressionClassificationRequest
-    }
-    
-    func makePredictions(for photo: CGImage, completionHandler: @escaping FaceExpressionPredictionHandler) throws {
-        let faceExpressionClassificaitonRequest = createFaceExpressionClassificationRequest(predictionHandler: completionHandler)
-        
-        let handler = VNImageRequestHandler(cgImage: photo)
-        let requests: [VNRequest] = [faceExpressionClassificaitonRequest]
-        
-        try handler.perform(requests)
     }
 }
