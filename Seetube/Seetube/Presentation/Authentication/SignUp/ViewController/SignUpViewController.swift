77d16cdec5337acf964a736593ad134cc0e2c62f
최수정 <babeeboo2000@gmail.com>
2023-02-28 00:53:49 +0900

    [Feat] #39 회원가입 뷰 구성

--- Seetube/Seetube/Presentation/Authentication/SignUp/ViewController/SignUpViewController.swift
+++ Seetube/Seetube/Presentation/Authentication/SignUp/ViewController/SignUpViewController.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    @IBOutlet weak var nicknameTextField: UnderLineTextField!
+    @IBOutlet weak var emailTextField: UnderLineTextField!
+    @IBOutlet weak var verificationCodeTextField: UnderLineTextField!
+    @IBOutlet weak var passwordTextField: UnderLineTextField!
+    @IBOutlet weak var passwordConfirmTextField: UnderLineTextField!
+    
+    @IBOutlet weak var nicknameValidationLabel: UILabel!
+    @IBOutlet weak var emailValidationLabel: UILabel!
+    @IBOutlet weak var verificationCodeValidationLabel: UILabel!
+    @IBOutlet weak var passwordValidationLabel: UILabel!
+    @IBOutlet weak var passwordConfirmValidationLabel: UILabel!
+    
+    @IBOutlet weak var verificationCodeRequestButton: UIButton!
+    @IBOutlet weak var signUpButton: BottomButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
