b758c3b5945b6a246945efc6bd62d9a606b63e56
최수정 <babeeboo2000@gmail.com>
2023-02-27 19:40:00 +0900

    [Feat] #39 SignUpViewController 생성

---
+++ Seetube/Seetube/Presentation/Authentication/SignUp/ViewController/SignUpViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewController.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/02/27.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
