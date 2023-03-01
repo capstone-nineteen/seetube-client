24d3cf59a0a7542b3ba2bab77f48c9de6451419d
최수정 <babeeboo2000@gmail.com>
2023-03-01 20:52:52 +0900

    [Feat] #42 SignInViewModel의 transform() 구현

--- Seetube/Seetube/Presentation/Authentication/SignIn/ViewModel/SignInViewModel.swift
+++ Seetube/Seetube/Presentation/Authentication/SignIn/ViewModel/SignInViewModel.swift
@@ -28,16 +28,49 @@ class SignInViewModel: ViewModelType {
             }
             .compactMap { $0 }
         
-        return Output(shouldMoveToSignUp: shouldMoveToSignUp)
+        let emailAndPassword = Driver
+            .combineLatest(
+                input.email,
+                input.password
+            ) { (email: $0, password: $1)}
+        
+        let signInResult = input.signInButtonTouched
+            .withLatestFrom(emailAndPassword) { $1 }
+            .flatMap { [weak self] (email: String, password: String) -> Driver<SignInResult?> in
+                guard let self = self else { return .just(nil) }
+                return self.signInUseCase
+                    .execute(userType: self.userType,
+                             email: email,
+                             password: password)
+                    .asDriver(onErrorJustReturn: nil)
+            }
+        
+        let signInSucceed = signInResult
+            .compactMap { $0 }
+            .map { [weak self] _ in self?.userType }
+            .compactMap { $0 }
+        
+        let signInFailed = signInResult
+            .filter { $0 == nil }
+            .mapToVoid()
+        
+        return Output(shouldMoveToSignUp: shouldMoveToSignUp,
+                      signInSucceed: signInSucceed,
+                      signInFailed: signInFailed)
     }
 }
 
 extension SignInViewModel {
     struct Input {
         let signUpButtonTouched: Driver<Void>
+        let email: Driver<String>
+        let password: Driver<String>
+        let signInButtonTouched: Driver<Void>
     }
     
     struct Output {
         let shouldMoveToSignUp: Driver<UserType>
+        let signInSucceed: Driver<UserType>
+        let signInFailed: Driver<Void>
     }
 }
