8c4d636507963d5889f5717e83dff9525571bb2e
최수정 <babeeboo2000@gmail.com>
2023-03-05 01:40:08 +0900

    [Feat] #44 ReviewsInProgressTableViewController 구현

--- Seetube/Seetube/Presentation/Youtuber/Home/ViewController/ReviewsInProgressTableViewController.swift
+++ Seetube/Seetube/Presentation/Youtuber/Home/ViewController/ReviewsInProgressTableViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ReviewsInProgressTableViewController: VideoInfoCardTableViewController, ViewControllerPushable {
+    fileprivate var viewModels = [YoutuberInProgressVideoCardItemViewModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,15 +21,17 @@ class ReviewsInProgressTableViewController: VideoInfoCardTableViewController, Vi
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
+        return self.viewModels.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ReviewsInProgressTableViewCell.cellReuseIdentifier, for: indexPath) as? ReviewsInProgressTableViewCell else { return UITableViewCell() }
+        // TODO: cell.bind(self.viewModels[indexPath.row])
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: 부모 뷰컨트롤러로 전달
         self.moveToVideoDetail()
     }
 }
@@ -33,5 +39,20 @@ class ReviewsInProgressTableViewController: VideoInfoCardTableViewController, Vi
 extension ReviewsInProgressTableViewController {
     private func moveToVideoDetail() {
         self.push(viewControllerType: YoutuberVideoDetailViewController.self)
+    }
+}
+
+// MARK: - Reactive Extension
+
+extension Reactive where Base: ReviewsInProgressTableViewController {
+    var viewModels: Binder<[YoutuberInProgressVideoCardItemViewModel]> {
+        return Binder(base) { (base, viewModels) in
+            base.viewModels = viewModels
+            base.tableView.reloadSections([0], with: .automatic)
+        }
+    }
+    
+    var itemSelected: ControlEvent<IndexPath> {
+        return base.tableView.rx.itemSelected
     }
 }
