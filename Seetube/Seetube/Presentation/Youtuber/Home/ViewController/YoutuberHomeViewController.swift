b1baf4129c61331cdd67778aca54cf6aa71e2660
최수정 <babeeboo2000@gmail.com>
2023-03-05 02:43:57 +0900

    [Refactor] #44 SegmentedControl Index 변경 감지 rx 방식으로 변경

--- Seetube/Seetube/Presentation/Youtuber/Home/ViewController/YoutuberHomeViewController.swift
+++ Seetube/Seetube/Presentation/Youtuber/Home/ViewController/YoutuberHomeViewController.swift
@@ -6,13 +6,19 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class YoutuberHomeViewController: UIViewController {
+    @IBOutlet weak var segmentedControl: UnderlineSegmentedControl!
     @IBOutlet weak var finishedReviewsView: UIView!
     @IBOutlet weak var reviewsInProgressView: UIView!
     
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,25 +30,30 @@ class YoutuberHomeViewController: UIViewController {
         super.viewWillDisappear(animated)
         self.navigationController?.isNavigationBarHidden = false
     }
-    
-    @IBAction func segmentedControlValueChanged(_ sender: UnderlineSegmentedControl) {
-        switch sender.selectedSegmentIndex {
-        case 0:
-            self.changeToFinishedReviewsTab()
-        case 1:
-            self.changeToReviewsInProgressTab()
-        default:
-            return
-        }
+}
+
+// MARK: - Configuration
+
+extension YoutuberHomeViewController {
+    private func configureUI() {
+        self.configureSegmentedControl()
     }
     
-    private func changeToFinishedReviewsTab() {
-        self.finishedReviewsView.isHidden = false
-        self.reviewsInProgressView.isHidden = true
-    }
-    
-    private func changeToReviewsInProgressTab() {
-        self.finishedReviewsView.isHidden = true
-        self.reviewsInProgressView.isHidden = false
+    private func configureSegmentedControl() {
+        self.segmentedControl.rx.selectedSegmentIndex
+            .asDriver()
+            .drive(with: self) { obj, index in
+                switch index {
+                case 0:
+                    obj.finishedReviewsView.isHidden = false
+                    obj.reviewsInProgressView.isHidden = true
+                case 1:
+                    obj.finishedReviewsView.isHidden = true
+                    obj.reviewsInProgressView.isHidden = false
+                default:
+                    return
+                }
+            }
+            .disposed(by: self.disposeBag)
     }
 }
