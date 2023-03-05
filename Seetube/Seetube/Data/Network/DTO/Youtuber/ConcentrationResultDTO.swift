b3137078bdd965e76d7876f458d700dfae321b15
최수정 <babeeboo2000@gmail.com>
2023-03-06 01:25:10 +0900

    [Feat] #46 ConcentrationResult 구현

--- Seetube/Seetube/Data/Network/DTO/Youtuber/ConcentrationResultDTO.swift
+++ Seetube/Seetube/Data/Network/DTO/Youtuber/ConcentrationResultDTO.swift
@@ -7,21 +7,28 @@
 
 import Foundation
 
-struct ConcentrationSceneDTO: Decodable {
+struct ConcentrationSceneDTO: Decodable, DomainConvertible {
     let thumbnailImageURL: String
     let startTime: Int
     let endTime: Int
     let totalNumberOfReviewers: Int
     let numberOfReviewersConcentrated: Int
+    
+    func toDomain() -> ConcentrationScene {
+        return ConcentrationScene(thumbnailImageURL: self.thumbnailImageURL,
+                                  startTime: self.startTime,
+                                  endTime: self.endTime,
+                                  totalNumberOfReviewers: self.totalNumberOfReviewers,
+                                  numberOfReviewersConcentrated: self.numberOfReviewersConcentrated)
+    }
 }
 
-struct ConcentrationResultDTO: Decodable {
+struct ConcentrationResultDTO: Decodable, DomainConvertible {
     let originalVideoURL: String
     let scenes: [ConcentrationSceneDTO]
-}
-
-extension ConcentrationResultDTO: DomainConvertible {
+    
     func toDomain() -> ConcentrationResult {
-        return ConcentrationResult()
+        return ConcentrationResult(originalVideoURL: self.originalVideoURL,
+                                   scenes: self.scenes.map { $0.toDomain() })
     }
 }
--- Seetube/Seetube/Domain/Model/Youtuber/ConcentrationResult.swift
+++ Seetube/Seetube/Domain/Model/Youtuber/ConcentrationResult.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+struct ConcentrationScene: Decodable {
+    let thumbnailImageURL: String
+    let startTime: Int
+    let endTime: Int
+    let totalNumberOfReviewers: Int
+    let numberOfReviewersConcentrated: Int
+}
+
 struct ConcentrationResult {
-    
+    let originalVideoURL: String
+    let scenes: [ConcentrationScene]
 }
