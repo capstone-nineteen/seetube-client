4ecd6e7f28301373d5736a2825fc90a38cc53ce6
최수정 <babeeboo2000@gmail.com>
2023-03-05 20:46:29 +0900

    [Refactor] #45 네비게이션바 설정 rx 방식으로 변경

--- Seetube/Seetube/Presentation/Youtuber/VideoDetail/ViewController/YoutuberVideoDetailViewController.swift
+++ Seetube/Seetube/Presentation/Youtuber/VideoDetail/ViewController/YoutuberVideoDetailViewController.swift
@@ -6,19 +6,38 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class YoutuberVideoDetailViewController: UIViewController {
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureUI()
+    }
+}
+
+// MARK: - Configuration
+
+extension YoutuberVideoDetailViewController {
+    private func configureUI() {
+        self.configureNavigationBar()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.prefersLargeTitles = false
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.navigationBar.prefersLargeTitles = true
+    private func configureNavigationBar() {
+        self.rx.viewWillAppear
+            .asDriver()
+            .drive(with: self) { obj, _ in
+                obj.navigationController?.navigationBar.prefersLargeTitles = false
+            }
+            .disposed(by: self.disposeBag)
+        
+        self.rx.viewWillDisappear
+            .asDriver()
+            .drive(with: self) { obj, _ in
+                obj.navigationController?.navigationBar.prefersLargeTitles = true
+            }
+            .disposed(by: self.disposeBag)
     }
 }
