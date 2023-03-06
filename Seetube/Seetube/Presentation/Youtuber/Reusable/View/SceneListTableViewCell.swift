7989ff4b456ce09f024353d1a9c85db6d8ad43fa
최수정 <babeeboo2000@gmail.com>
2023-03-06 19:16:32 +0900

    [Feat] #46 SceneListTableViewCell bind 함수 구현

--- Seetube/Seetube/Presentation/Youtuber/Reusable/View/SceneListTableViewCell.swift
+++ Seetube/Seetube/Presentation/Youtuber/Reusable/View/SceneListTableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class SceneListTableViewCell: UITableViewCell {
     static let cellReuseIdentifier: String = "SceneListTableViewCell"
@@ -16,7 +17,36 @@ class SceneListTableViewCell: UITableViewCell {
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var progressBar: CircularProgressBar!
     
-    func setProgress(value: Double, text: String?, color: UIColor?) {
-        self.progressBar.setProgress(value: value, text: text ?? "", color: color)
+    private func bindImage(url: String) {
+        let processor = DownsamplingImageProcessor(size: self.thumbnailImageView.bounds.size)
+        self.thumbnailImageView.kf.indicatorType = .activity
+        self.thumbnailImageView.kf.setImage(
+            with: URL(string: url),
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(0.7)),
+                .cacheOriginalImage
+            ]
+        )
+    }
+    
+    func bind(_ viewModel: SceneItemViewModel) {
+        self.bindImage(url: viewModel.thumbnailUrl)
+        self.timeIntervalLabel.text = viewModel.interval
+        self.contentLabel.text = viewModel.description
+        self.progressBar.setProgress(value: viewModel.progress,
+                                     text: viewModel.progressDescription,
+                                     color: Colors.seetubePink)
+        // TODO: 프로그레스바 색상 변경
+    }
+    
+    func clear() {
+        self.thumbnailImageView.kf.cancelDownloadTask()
+        self.timeIntervalLabel.text = nil
+        self.contentLabel.text = nil
+        self.progressBar.setProgress(value: 0,
+                                     text: "",
+                                     color: nil)
     }
 }
