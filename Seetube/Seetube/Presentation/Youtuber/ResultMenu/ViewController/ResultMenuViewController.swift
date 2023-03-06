c574f91f027f421c9cdb2e6faeda7738df21f18f
최수정 <babeeboo2000@gmail.com>
2023-03-06 19:18:54 +0900

    [Refactor] #46 집중도 결과화면 전환 rx 방식으로 변경

--- Seetube/Seetube/Presentation/Youtuber/ResultMenu/ViewController/ResultMenuViewController.swift
+++ Seetube/Seetube/Presentation/Youtuber/ResultMenu/ViewController/ResultMenuViewController.swift
@@ -6,11 +6,36 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class ResultMenuViewController: UIViewController {
+class ResultMenuViewController: UIViewController,
+                                ConcentrationResultPushable
+{
+    @IBOutlet weak var concentrationButton: ResultMenuButton!
+    
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureUI()
+    }
+}
 
-        
+// MARK: - Configuration
+
+extension ResultMenuViewController {
+    private func configureUI() {
+        self.configureConcentrationButton()
+    }
+    
+    private func configureConcentrationButton() {
+        self.concentrationButton.rx.tap
+            .asDriver()
+            .drive(with: self) { obj, _ in
+                // TODO: videoId 주입
+                obj.pushConcentrationResult(videoId: 0)
+            }
+            .disposed(by: self.disposeBag)
     }
 }
