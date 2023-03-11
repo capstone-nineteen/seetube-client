0d20947862d1cc173a5ae90222d57bfa3d912db0
최수정 <babeeboo2000@gmail.com>
2023-03-12 02:47:09 +0900

    [Feat] #48 SceneStealerResultViewController 의존성 주입

--- Seetube/Seetube/Presentation/Youtuber/ResultMenu/ViewController/ResultMenuViewController.swift
+++ Seetube/Seetube/Presentation/Youtuber/ResultMenu/ViewController/ResultMenuViewController.swift
@@ -11,10 +11,12 @@ import RxSwift
 
 class ResultMenuViewController: UIViewController,
                                 ConcentrationResultPushable,
-                                EmotionResultPushable
+                                EmotionResultPushable,
+                                SceneStealerResultPushable
 {
     @IBOutlet weak var concentrationButton: ResultMenuButton!
     @IBOutlet weak var emotionButton: ResultMenuButton!
+    @IBOutlet weak var sceneStealerButton: ResultMenuButton!
     
     private var disposeBag = DisposeBag()
     
@@ -30,6 +32,7 @@ extension ResultMenuViewController {
     private func configureUI() {
         self.configureConcentrationButton()
         self.configureEmotionButton()
+        self.configureSceneStealerButton()
     }
     
     private func configureConcentrationButton() {
@@ -48,6 +51,16 @@ extension ResultMenuViewController {
             .drive(with: self) { obj, _ in
                 // TODO: videoId 주입
                 obj.pushEmotionResult(videoId: 0)
+            }
+            .disposed(by: self.disposeBag)
+    }
+    
+    private func configureSceneStealerButton() {
+        self.sceneStealerButton.rx.tap
+            .asDriver()
+            .drive(with: self) { obj, _ in
+                // TODO: videoId 주입
+                obj.pushSceneStealerResult(videoId: 0)
             }
             .disposed(by: self.disposeBag)
     }
