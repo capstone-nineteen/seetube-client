d3866df723488bc0f2c3711ff8e308c82c408be6
최수정 <babeeboo2000@gmail.com>
2023-03-12 19:23:35 +0900

    [Feat] #49 HighlightResultViewModel 구현

---
+++ Seetube/Seetube/Presentation/Youtuber/HighlightResult/ViewModel/HighlightResultViewModel.swift
@@ -0,0 +1,73 @@
+//
+//  HighlightResultViewModel.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/03/12.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+class HighlightResultViewModel: ViewModelType {
+    private let videoId: Int
+    private let fetchHighlightResultUseCase: FetchHighlightResultUseCase
+    
+    init(
+        videoId: Int,
+        fetchHighlightResultUseCase: FetchHighlightResultUseCase
+    ) {
+        self.videoId = videoId
+        self.fetchHighlightResultUseCase = fetchHighlightResultUseCase
+    }
+    
+    func transform(input: Input) -> Output {
+        let result = input.viewWillAppear
+            .flatMap { [weak self] _ -> Driver<HighlightResult?> in
+                // 더미 데이터
+                let scenes = (0..<5).map {
+                    ConcentrationScene(thumbnailImageURL: "https://avatars.githubusercontent.com/u/70833900?s=80&u=7b4aff238820c6e6d3968848b78e8d8bf1b1507e&v=4",
+                                                   startTime: 5*$0,
+                                                   endTime: 5*$0+5,
+                                                   totalNumberOfReviewers: 200,
+                                                   numberOfReviewersConcentrated: 162)
+                }
+                let temp = ConcentrationResult(originalVideoURL: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                                               scenes: scenes)
+                
+                guard let self = self else { return .just(nil) }
+                return self.fetchHighlightResultUseCase
+                    .execute(videoId: self.videoId)
+                    .asDriver(onErrorJustReturn: nil)
+            }
+            .compactMap { $0 }
+        
+        let videoUrl = result
+            .map { $0.highlightVideoURL }
+        
+        let scenes = result
+            .map { $0.scenes }
+            .map { $0.map { SceneLargeItemViewModel(with: $0) } }
+        
+        let playingInterval = input.itemSelected
+            .withLatestFrom(result) { $1.scenes[$0.row] }
+            .map { (start: $0.startTimeInHighlight, end: $0.endTimeInHighlight) }
+        
+        return Output(videoUrl: videoUrl,
+                      scenes: scenes,
+                      playingInterval: playingInterval)
+    }
+}
+
+extension HighlightResultViewModel {
+    struct Input {
+        let viewWillAppear: Driver<Bool>
+        let itemSelected: Driver<IndexPath>
+    }
+    
+    struct Output {
+        let videoUrl: Driver<String>
+        let scenes: Driver<[SceneLargeItemViewModel]>
+        let playingInterval: Driver<(start: Int, end: Int)>
+    }
+}
---
+++ Seetube/Seetube/Presentation/Youtuber/HighlightResult/ViewModel/SceneLargeItemViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  SceneLargeItemViewModel.swift
+//  Seetube
+//
+//  Created by 최수정 on 2023/03/12.
+//
+
+import Foundation
+
+class SceneLargeItemViewModel {
+    let thumbnailUrl: String
+    let interval: String
+    let description: String
+    
+    init(
+        thumbnailUrl: String,
+        interval: String,
+        description: String
+    ) {
+        self.thumbnailUrl = thumbnailUrl
+        self.interval = interval
+        self.description = description
+    }
+    
+    convenience init(with scene: HighlightScene) {
+        let interval = StringFormattingHelper.toTimeIntervalFormatString(startSecond: scene.startTimeInOriginalVideo,
+                                                                         endSecond: scene.endTimeInOriginalVideo)
+        let description = "총 \(scene.totalNumberOfReviewers)명 중에\n\(scene.numberOfReviewersConcentrated)이 집중했고,\n\(scene.numberOfReviewersFelt)이 \(scene.emotionType.korDescription)을(를) 느꼈습니다."
+        
+        self.init(thumbnailUrl: scene.thumbnailImageURL,
+                  interval: interval,
+                  description: description)
+    }
+}
