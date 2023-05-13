e9f8b60bc4f5ce0302f415fd1d47befd0e944363
최수정 <babeeboo2000@gmail.com>
2023-05-13 15:58:29 +0900

    [Fix] #49 하이라이트 API 수정 반영

--- Seetube/Seetube/Data/Network/DTO/Youtuber/HighlightResultDTO.swift
+++ Seetube/Seetube/Data/Network/DTO/Youtuber/HighlightResultDTO.swift
@@ -7,36 +7,121 @@
 
 import Foundation
 
-struct HighlightSceneDTO: Decodable, DomainConvertible {
-    let thumbnailImageURL: String
-    let startTimeInOriginalVideo: Int
-    let endTimeInOriginalVideo: Int
-    let startTimeInHighlight: Int
-    let endTimeInHighlight: Int
-    let totalNumberOfReviewers: Int
-    let numberOfReviewersConcentrated: Int
-    let numberOfReviewersFelt: Int
-    let emotionType: Emotion
+struct HighlightsDTO: Decodable, DomainConvertible {
+    let thumbnailURL: String
+    let numOfTotalReviewers: Int
     
-    func toDomain() -> HighlightScene {
-        return HighlightScene(thumbnailImageURL: self.thumbnailImageURL,
-                                    startTimeInOriginalVideo: self.startTimeInOriginalVideo,
-                                    endTimeInOriginalVideo: self.endTimeInOriginalVideo,
-                                    startTimeInHighlight: self.startTimeInHighlight,
-                                    endTimeInHighlight: self.endTimeInHighlight,
-                                    totalNumberOfReviewers: self.totalNumberOfReviewers,
-                                    numberOfReviewersConcentrated: self.numberOfReviewersConcentrated,
-                                    numberOfReviewersFelt: self.numberOfReviewersFelt,
-                                    emotionType: self.emotionType)
+    let firstSceneStartTimeInOriginalVideo: Int
+    let firstSceneEndTimeInOriginalVideo: Int
+    let firstSceneStartTimeInHighlight: Int
+    let firstSceneEndTimeInHighlight: Int
+    let numberOfReviewersConcentratedInFirstScene: Int
+    let numberOfReviewersFeltInFirstScene: Int
+    let emotionTypeInFirstScene: String
+    
+    let secondSceneStartTimeInOriginalVideo: Int?
+    let secondSceneEndTimeInOriginalVideo: Int?
+    let secondSceneStartTimeInHighlight: Int?
+    let secondSceneEndTimeInHighlight: Int?
+    let numberOfReviewersConcentratedInSecondScene: Int?
+    let numberOfReviewersFeltInSecondScene: Int?
+    let emotionTypeInSecondScene: String?
+    
+    let thirdSceneStartTimeInOriginalVideo: Int?
+    let thirdSceneEndTimeInOriginalVideo: Int?
+    let thirdSceneStartTimeInHighlight: Int?
+    let thirdSceneEndTimeInHighlight: Int?
+    let numberOfReviewersConcentratedInThirdScene: Int?
+    let numberOfReviewersFeltInThirdScene:Int?
+    let emotionTypeInThirdScene: String?
+    
+    let fourthSceneStartTimeInOriginalVideo:Int?
+    let fourthSceneEndTimeInOriginalVideo: Int?
+    let fourthSceneStartTimeInHighlight: Int?
+    let fourthSceneEndTimeInHighlight: Int?
+    let numberOfReviewersConcentratedInFourthScene: Int?
+    let numberOfReviewersFeltInFourthScene: Int?
+    let emotionTypeInFourthScene: String?
+    
+    let fifthSceneStartTimeInOriginalVideo: Int?
+    let fifthSceneEndTimeInOriginalVideo: Int?
+    let fifthSceneStartTimeInHighlight: Int?
+    let fifthSceneEndTimeInHighlight: Int?
+    let numberOfReviewersConcentratedInFifthScene: Int?
+    let numberOfReviewersFeltInFifthScene: Int?
+    let emotionTypeInFifthScene: String?
+    
+    func toDomain() -> [HighlightScene] {
+        let maxNumberOfScenes = 5
+        
+        let thumbnailImageURLs: [String?] = []
+        let startTimesInOriginalVideo = [firstSceneStartTimeInOriginalVideo,
+                                         secondSceneStartTimeInOriginalVideo,
+                                         thirdSceneStartTimeInOriginalVideo,
+                                         fourthSceneStartTimeInOriginalVideo,
+                                         fifthSceneStartTimeInOriginalVideo]
+        let endTimesInOriginalVideo = [firstSceneEndTimeInOriginalVideo,
+                                       secondSceneEndTimeInOriginalVideo,
+                                       thirdSceneEndTimeInOriginalVideo,
+                                       fourthSceneEndTimeInOriginalVideo,
+                                       fifthSceneEndTimeInOriginalVideo]
+        let startTimesInHighlight = [firstSceneStartTimeInHighlight,
+                                     secondSceneStartTimeInHighlight,
+                                     thirdSceneStartTimeInHighlight,
+                                     fourthSceneStartTimeInHighlight,
+                                     fifthSceneStartTimeInHighlight]
+        let endTimesInHighlight = [firstSceneEndTimeInHighlight,
+                                   secondSceneEndTimeInHighlight,
+                                   thirdSceneEndTimeInHighlight,
+                                   fourthSceneEndTimeInHighlight,
+                                   fifthSceneEndTimeInHighlight]
+        let numbersOfReviewersConcentrated = [numberOfReviewersConcentratedInFirstScene,
+                                              numberOfReviewersConcentratedInSecondScene,
+                                              numberOfReviewersConcentratedInThirdScene,
+                                              numberOfReviewersConcentratedInFourthScene,
+                                              numberOfReviewersConcentratedInFifthScene]
+        let numbersOfReviewersFelt = [numberOfReviewersFeltInFirstScene,
+                                      numberOfReviewersFeltInSecondScene,
+                                      numberOfReviewersFeltInThirdScene,
+                                      numberOfReviewersFeltInFourthScene,
+                                      numberOfReviewersFeltInFifthScene]
+        let emotionTypes = [emotionTypeInFirstScene,
+                            emotionTypeInSecondScene,
+                            emotionTypeInThirdScene,
+                            emotionTypeInFourthScene,
+                            emotionTypeInFifthScene]
+        
+        return (0..<maxNumberOfScenes)
+            .map { index -> HighlightScene? in
+                guard let thumbnailImageURL = thumbnailImageURLs[index],
+                      let startTimeInOriginalVideo = startTimesInOriginalVideo[index],
+                      let endTimeInOriginalVideo = endTimesInOriginalVideo[index],
+                      let startTimeInHighlight = startTimesInHighlight[index],
+                      let endTimeInHighlight = endTimesInHighlight[index],
+                      let numberOfReviewersConcentrated = numbersOfReviewersConcentrated[index],
+                      let numberOfReviewersFelt = numbersOfReviewersFelt[index],
+                      let emotionType = emotionTypes[index] else { return nil }
+                
+                return HighlightScene(thumbnailImageURL: thumbnailImageURL,
+                                      startTimeInOriginalVideo: startTimeInOriginalVideo,
+                                      endTimeInOriginalVideo: endTimeInOriginalVideo,
+                                      startTimeInHighlight: startTimeInHighlight,
+                                      endTimeInHighlight: endTimeInHighlight,
+                                      totalNumberOfReviewers: self.numOfTotalReviewers,
+                                      numberOfReviewersConcentrated: numberOfReviewersConcentrated,
+                                      numberOfReviewersFelt: numberOfReviewersFelt,
+                                      emotionType: Emotion(rawValue: emotionType) ?? .neutral)
+            }
+            .compactMap{ $0 }
     }
 }
 
 struct HighlightResultDTO: Decodable, DomainConvertible {
-    let highlightVideoURL: String
-    let scenes: [HighlightSceneDTO]
+    let originalVideoURL: String
+    let highlight: HighlightsDTO
     
     func toDomain() -> HighlightResult {
-        return HighlightResult(highlightVideoURL: self.highlightVideoURL,
-                               scenes: self.scenes.map { $0.toDomain() })
+        return HighlightResult(highlightVideoURL: self.originalVideoURL,
+                               scenes: self.highlight.toDomain())
     }
 }
